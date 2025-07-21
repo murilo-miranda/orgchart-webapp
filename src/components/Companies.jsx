ffed12276959b5a1be25d91ef6f2b07352f9e89c
= <=>
2025-07-21 15:07:51 -0300

    Make list company functional

---
+++ src/components/Companies.jsx
@@ -0,0 +1,26 @@
+import styles from "./Companies.module.css";
+import { useQuery } from "@apollo/client";
+import { GET_COMPANIES } from "../graphql/companies.query";
+
+export function Companies() {
+  const { loading, error, data } = useQuery(GET_COMPANIES);
+
+  if (error) return <p>Ocorreu um erro ao carregar empresas</p>;
+  if (loading) return <p>Carregando empresas...</p>;
+  if (data?.companies.length === 0) {
+    return <p>Não existem empresas cadastradas</p>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Empresas</h1>
+      <ul className={styles.list}>
+        {data.companies.map((company) => (
+          <li key={company.id} className={styles.companyItem}>
+            {company.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
---
+++ src/components/Companies.test.jsx
@@ -0,0 +1,67 @@
+import '@testing-library/jest-dom'
+import {render, screen} from '@testing-library/react'
+import { Companies } from './Companies'
+import { MockedProvider } from '@apollo/client/testing';
+import { describe, expect } from 'vitest';
+import {
+  companiesMocksEmpty,
+  companiesMocksError,
+  companiesMocksSuccess
+} from './mocks/companies.mock';
+
+describe('Companies Component', () => {
+  describe('When content is loading', () => {
+    test("it renders loading message", async () => {
+      render(
+        <MockedProvider mocks={[]}>
+          <Companies />
+        </MockedProvider>
+      );
+
+      expect(screen.getByText("Carregando empresas...")).toBeInTheDocument();
+    });
+  });
+
+  describe('When companies exist', () => {
+    test("it renders companies name", async () => {
+      render(
+        <MockedProvider mocks={companiesMocksSuccess}>
+          <Companies />
+        </MockedProvider>
+      );
+
+      const h1 = await screen.findByText('Empresas');
+      const uol = await screen.findByText('Uol');
+      const uolTech = await screen.findByText('Uol Tech');
+      expect(h1).toBeInTheDocument();
+      expect(uol).toBeInTheDocument();
+      expect(uolTech).toBeInTheDocument();
+    });
+  });
+
+  describe('When companies does not exist', () => {
+    test("it renders empty message", async () => {
+      render(
+        <MockedProvider mocks={companiesMocksEmpty}>
+          <Companies />
+        </MockedProvider>
+      );
+
+      const message = await screen.findByText("Não existem empresas cadastradas");
+      expect(message).toBeInTheDocument();
+    });
+  });
+
+  describe('When api is not reachable', () => {
+    test("it renders error message", async () => {
+      render(
+        <MockedProvider mocks={companiesMocksError}>
+          <Companies />
+        </MockedProvider>
+      );
+
+      const errorMessage = await screen.findByText("Ocorreu um erro ao carregar empresas");
+      expect(errorMessage).toBeInTheDocument();
+    });
+  });
+});
