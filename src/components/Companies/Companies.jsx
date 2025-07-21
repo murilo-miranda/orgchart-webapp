d460f43327e9ed0ed24abe39ac3ce6ea0d310a98
Murilo Miranda <murilomirandamoreno@gmail.com>
2025-07-21 20:46:52 -0300

    Extract request from component (#4)
    - Move request from component to page

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -1,14 +1,10 @@
 import { Link } from "react-router-dom";
 import styles from "./Companies.module.css";
-import { useQuery } from "@apollo/client";
-import { GET_COMPANIES } from "../../graphql/company/queries/companies.query";
 
-export function Companies() {
-  const { loading, error, data } = useQuery(GET_COMPANIES);
-
+export function Companies({ loading, error, companies }) {
   if (error) return <p>Ocorreu um erro ao carregar empresas</p>;
   if (loading) return <p>Carregando empresas...</p>;
-  if (data?.companies.length === 0) {
+  if (companies.length === 0) {
     return <p>Não existem empresas cadastradas</p>;
   }
 
@@ -16,7 +12,7 @@ export function Companies() {
     <div className={styles.container}>
       <h1 className={styles.title}>Empresas</h1>
       <ul className={styles.list}>
-        {data.companies.map((company) => (
+        {companies.map((company) => (
           <li key={company.id} className={styles.companyItem}>
             <Link to={`/company/${company.id}`}>{company.name}</Link>
           </li>
